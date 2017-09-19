9de9a6a6ee044f6584fe9d2e70967b07e638a10b
Toby Hughes <tobywhughes@gmail.com>
2017-09-19 11:44:29 -0700

    Update Program.cs
    i fixed it more im great

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -19,7 +19,7 @@ namespace TicTacToe
             do
             {
                 Console.Clear();
-                Console.WriteLine("Player 1: X /n Player 2: O\n\n");
+                Console.WriteLine("Player 1: X\tPlayer 2: O\n\n");
 
                 if (player % 2 == 0)
                 {
